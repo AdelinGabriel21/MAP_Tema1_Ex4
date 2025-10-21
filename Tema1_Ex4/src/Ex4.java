73f0946d540d2d55f03289bdc150f9fa50807376
AdelinGabriel21 <116873929+AdelinGabriel21@users.noreply.github.com>
2025-10-21 13:00:31 +0300

    cheapestKeyboard added

--- Tema1_Ex4/src/Ex4.java
+++ Tema1_Ex4/src/Ex4.java
@@ -1,2 +1,11 @@
 public class Ex4 {
+    public int cheapestKeyboard(int[] keyboards) {
+        int min = keyboards[0];
+        for (int cost : keyboards) {
+            if (cost < min) {
+                min = cost;
+            }
+        }
+        return min;
+    }
 }
--- Tema1_Ex4/src/Main.java
+++ Tema1_Ex4/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Ex4 ex4 = new Ex4();
 
+        int[] keyboards = {40, 35, 70, 15, 45};
 
+        System.out.println("Cheapest Keyboard: " + ex4.cheapestKeyboard(keyboards));
     }
 }
