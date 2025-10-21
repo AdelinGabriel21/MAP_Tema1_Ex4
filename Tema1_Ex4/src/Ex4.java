b8ebcd2002ebb15f4b1e87c6a44cf2532b0241c5
AdelinGabriel21 <116873929+AdelinGabriel21@users.noreply.github.com>
2025-10-21 13:09:01 +0300

    mostExpensiveItem + mostExpensiveUSB added

--- Tema1_Ex4/src/Ex4.java
+++ Tema1_Ex4/src/Ex4.java
@@ -8,4 +8,29 @@ public class Ex4 {
         }
         return min;
     }
+
+    public int mostExpensiveItem(int[] keyboards, int[] usb) {
+        int max = keyboards[0];
+        for (int cost : keyboards) {
+            if (cost > max) {
+                max = cost;
+            }
+        }
+        for (int cost : usb) {
+            if (cost > max) {
+                max = cost;
+            }
+        }
+        return max;
+    }
+
+    public int mostExpensiveUSB(int[] usb, int budget){
+        int max = usb[0];
+        for (int cost : usb) {
+            if (cost <= budget && cost > max) {
+                max = cost;
+            }
+        }
+        return max;
+    }
 }
--- Tema1_Ex4/src/Main.java
+++ Tema1_Ex4/src/Main.java
@@ -3,7 +3,13 @@ public class Main {
         Ex4 ex4 = new Ex4();
 
         int[] keyboards = {40, 35, 70, 15, 45};
+        int[] keyboards2 = {15, 20, 10, 35};
+        int[] usb = {20, 15, 40, 15};
+        int[] usb2 = {15, 45, 20};
+        int budget = 30;
 
         System.out.println("Cheapest Keyboard: " + ex4.cheapestKeyboard(keyboards));
+        System.out.println("Most expensive item: " + ex4.mostExpensiveItem(keyboards2, usb));
+        System.out.println("Most expensive USB Markus can buy: " + ex4.mostExpensiveUSB(usb2, budget));
     }
 }
